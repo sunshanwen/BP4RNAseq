9f9e6bd3c9b4623878247e576770641d1ca08054
Shanwen Sun <sunshanwen@gmail.com>
2020-05-05 23:16:10 +0800

    changed the code to download the reference genome based on the official documentation of datasets

--- R/down_Ref.R
+++ R/down_Ref.R
@@ -10,11 +10,14 @@
 #' down_Ref("sesame")
 #'}
 down_Ref <- function(taxa) {
-  cmd1 <- paste("./datasets assembly_descriptors tax_name", taxa, "-r | jq .datasets[].assembly_accession -r")
+  #cmd1 <- paste("./datasets assembly_descriptors tax_name", taxa, "-r | jq .datasets[].assembly_accession -r") ### change "_" to "-" according to the official documentation of datasets
+  taxa <- paste0("\"",taxa,"\"")
+  cmd1 <- paste("./datasets assembly-descriptors tax-name", taxa, "--refseq --assmaccs | jq .datasets[].assembly_accession -r") ### may change later
   # cat(cmd1, "\n")
   accession_id <- system(cmd1,  intern = TRUE)
   # print(accession_id)
-  cmd2 <- paste("./datasets download assembly", accession_id, "--include_gff3 --include_rna")
+  #cmd2 <- paste("./datasets download assembly", accession_id, "--include_gff3 --include_rna") ### change "_" to "-" according to the official documentation of datasets
+  cmd2 <- paste("./datasets download assembly", accession_id, "--include-gff3 --include-rna")
   # cat(cmd2, "\n")
   system(cmd2)
 }
