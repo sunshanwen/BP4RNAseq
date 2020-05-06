2c674a1e3292fcfa1fb0d79cda70d4410b29cb70
Shanwen Sun <sunshanwen@gmail.com>
2020-05-06 14:06:17 +0800

    add codes to ensure that annotation and transcript data are downloaded.

--- R/down_Ref.R
+++ R/down_Ref.R
@@ -15,23 +15,31 @@ down_Ref <- function(taxa) {
   cmd1 <- paste("./datasets assembly-descriptors tax-name", taxa, "--refseq --assmaccs | jq .datasets[].assembly_accession -r") ### may change later
   # cat(cmd1, "\n")
   accession_id <- system(cmd1,  intern = TRUE)
-  size1 <- paste("./datasets assembly-descriptors tax-name", taxa,"--refseq | jq '.datasets[].annotation_metadata.file[] | select(.type == \"GENOME_GFF\") | .estimated_size' -r")
-  #cat(size1)
-  annotation_size <- system(size1, intern = TRUE)
-  size2 <- paste("./datasets assembly-descriptors tax-name", taxa,"--refseq | jq '.datasets[].annotation_metadata.file[] | select(.type == \"RNA_FASTA\") | .estimated_size' -r")
-  transcript_size <- system(size2, intern = TRUE)
-  size3 <- paste("./datasets assembly-descriptors tax-name", taxa,"--refseq | jq '.datasets[].estimated_size' -r")
-  genome_size <- system(size3, intern = TRUE)
-  all_size <- annotation_size + transcript_size + genome_size
-  continue <- TURE
+  # size1 <- paste("./datasets assembly-descriptors tax-name", taxa,"--refseq | jq '.datasets[].annotation_metadata.file[] | select(.type == \"GENOME_GFF\") | .estimated_size' -r")
+  # #cat(size1)
+  # annotation_size <- system(size1, intern = TRUE)
+  #
+  # size2 <- paste("./datasets assembly-descriptors tax-name", taxa,"--refseq | jq '.datasets[].annotation_metadata.file[] | select(.type == \"RNA_FASTA\") | .estimated_size' -r")
+  # transcript_size <- system(size2, intern = TRUE)
+  # size3 <- paste("./datasets assembly-descriptors tax-name", taxa,"--refseq | jq '.datasets[].estimated_size' -r")
+  # genome_size <- system(size3, intern = TRUE)
+  # all_size <- as.numeric(annotation_size) + as.numeric(transcript_size) + as.numeric(genome_size)
+  # print(c(all_size, annotation_size, transcript_size, genome_size))
+
+  continue <- TRUE
   while(continue){
     # print(accession_id)
-    #cmd2 <- paste("./datasets download assembly", accession_id, "--include_gff3 --include_rna") ### change "_" to "-" according to the official documentation of datasets
-    cmd2 <- paste("./datasets download assembly", accession_id, "--include-gff3 --include-rna")
+    # cmd2 <- paste("./datasets download assembly", accession_id, "--include_gff3 --include_rna") ### change "_" to "-" according to the official documentation of datasets
+      cmd2 <- paste("./datasets download assembly", accession_id, "--include-gff3 --include-rna")
     # cat(cmd2, "\n")
+    # cmd2 <- paste("./datasets download assembly", accession_id, "--unresolved")
+
     system(cmd2)
     file <- list.files(pattern = "^ncbi.*zip$")
-    file_size <- file.info(file)$size
-    if(file_size == all_size) continue = FALSE
+    files <- paste(unzip(file, list = TRUE)$Name, collapse = " ")
+    if(grepl("rna.fna", files) && grepl("genomic.gff", files)) {continue = FALSE}
+    # files <- gsub("[0-9a-zA-Z.]*$","", unzip(file, list = TRUE)$Name)
+    # print(files)
+    # if("rna.fna" %in% files && "genomic.gff" %in% files) {continue = FALSE}
   }
 }
