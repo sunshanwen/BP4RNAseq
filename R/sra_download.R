42521ad920ee465b3f6243da7a64b38a142f199e
sunshanwen <sunshanwen@gmail.com>
2020-08-21 19:36:05 +0800

    warn when SRA Toolkit and Entrez Direct tool are not avaliable

--- R/sra_download.R
+++ R/sra_download.R
@@ -12,104 +12,171 @@
 
 sra_download <- function(accession, dir = getwd())
 {
-  system(
-    paste("/bin/bash -c",
-          shQuote("vdb-config -i & read -t 3; kill $!")),
-    ignore.stdout = TRUE,
-    ignore.stderr = TRUE
-  )
-  ## Get the default download directory
-  dir.download <- NULL
-
-  root.dir <-
-    system("vdb-config | grep '<root>' | cut -d '>' -f 2 | cut -d '<' -f 1",
-           intern = TRUE)
-  root.exist <- NULL
-  for (i in seq_len(length(root.dir))) {
-    if (root.dir[i] != ".") {
-      dir.download = root.dir[i]
-      root.exist <- TRUE
-    }
-  }
-  if (length(dir.download) == 0) {
-    dir.download <-
-      system("vdb-config | grep '<HOME>' | cut -d '>' -f 2 | cut -d '<' -f 1",
-             intern = TRUE)
-    root.exist <- FALSE
-  }
-  #### start downloading
-  file_path <- NULL
-  status <- NULL
-  if (root.exist == TRUE) {
-    for (f in accession)
-    {
-      #### use Entrez Direct tool to get all samples accession code, then use prefetch to download all the data
-      cmd2 = paste(
-        "$(esearch -db sra -query",
-        f,
-        "| efetch --format runinfo | grep",
-        f,
-        "| cut -d \",\" -f 1)"
-      )
-      status <-
-        tryCatch(
-          system2("prefetch", args= cmd2, stdout = FALSE, stderr = FALSE),
-          error = function(err) {1},
-          warning = function(war){2}
-        )     
-      if (status == 0) {
-        file_path <- paste0(dir.download, "/sra")
-        file <-
-          list.files(
-            file_path,
-            pattern = ".sra$",
-            recursive = FALSE,
-            full.names = TRUE
-          )
-        # file<-list.files("./sra", pattern = ".sra$", recursive = F, full.names = T)
-        files <- paste(file,collapse=" ")
-        cmd4 <- paste("mv", files, "-t", dir)
-        system(cmd4)
-      }
-    }
-    unlink(file_path)
+  if (Sys.info()['sysname'] == "Windows") {
+    print("Please use Windows subsystem for linux.")
+    status <- 1
   } else{
-    for (f in accession)
-    {
-      #### use Entrez Direct tool to get all samples accession code, then use prefetch to download all the data
-      cmd2 = paste(
-        "$(esearch -db sra -query",
-        f,
-        "| efetch --format runinfo | grep",
-        f,
-        "| cut -d \",\" -f 1)"
+    ### check if sratools is available or not.
+    status <-
+      tryCatch(
+        system2(
+          "which",
+          args = shQuote("vdb-config"),
+          stdout = FALSE,
+          stderr = FALSE
+        ),
+        error = function(err) {
+          1
+        },
+        warning = function(war) {
+          2
+        }
       )
+    if (status == 0) {
+      system(
+        paste(
+          "/bin/bash -c",
+          shQuote("vdb-config --interactive & read -t 3; kill $!")
+        ),
+        ignore.stdout = TRUE,
+        ignore.stderr = TRUE
+      )
+
+      ## Get the default download directory
+      dir.download <- NULL
+
+      root.dir <-
+        system("vdb-config | grep '<root>' | cut -d '>' -f 2 | cut -d '<' -f 1",
+               intern = TRUE)
+      root.exist <- NULL
+      for (i in seq_len(length(root.dir))) {
+        if (root.dir[i] != ".") {
+          dir.download = root.dir[i]
+          root.exist <- TRUE
+        }
+      }
+      if (length(dir.download) == 0) {
+        dir.download <-
+          system("vdb-config | grep '<HOME>' | cut -d '>' -f 2 | cut -d '<' -f 1",
+                 intern = TRUE)
+        root.exist <- FALSE
+      }
+
+      ### check if Entrez Direct tool is avaliable
       status <-
         tryCatch(
-          system2("prefetch", args= cmd2, stdout = FALSE, stderr = FALSE),
-          error = function(err) {1},
-          warning = function(war){2}
-        )     
+          system2(
+            "which",
+            args = shQuote("esearch"),
+            stdout = FALSE,
+            stderr = FALSE
+          ),
+          error = function(err) {
+            1
+          },
+          warning = function(war) {
+            2
+          }
+        )
+      #### start downloading
       if (status == 0) {
-        file_path <- paste0(dir.download, "/", f)
-        # print(file_path)
-        file <-
-          list.files(
-            file_path,
-            pattern = ".sra$",
-            recursive = FALSE,
-            full.names = TRUE
+        file_path <- NULL
+        status <- NULL
+        if (root.exist == TRUE) {
+          for (f in accession)
+          {
+            #### use Entrez Direct tool to get all samples accession code, then use prefetch to download all the data
+            cmd2 = paste(
+              "$(esearch -db sra -query",
+              f,
+              "| efetch --format runinfo | grep",
+              f,
+              "| cut -d \",\" -f 1)"
+            )
+            status <-
+              tryCatch(
+                system2(
+                  "prefetch",
+                  args = cmd2,
+                  stdout = FALSE,
+                  stderr = FALSE
+                ),
+                error = function(err) {
+                  1
+                },
+                warning = function(war) {
+                  2
+                }
+              )
+            if (status == 0) {
+              file_path <- paste0(dir.download, "/sra")
+              file <-
+                list.files(
+                  file_path,
+                  pattern = ".sra$",
+                  recursive = FALSE,
+                  full.names = TRUE
+                )
+              # file<-list.files("./sra", pattern = ".sra$", recursive = F, full.names = T)
+              files <- paste(file, collapse = " ")
+              cmd4 <- paste("mv", files, "-t", dir)
+              system(cmd4)
+            }
+          }
+          unlink(file_path)
+        } else{
+          for (f in accession)
+          {
+            #### use Entrez Direct tool to get all samples accession code, then use prefetch to download all the data
+            cmd2 = paste(
+              "$(esearch -db sra -query",
+              f,
+              "| efetch --format runinfo | grep",
+              f,
+              "| cut -d \",\" -f 1)"
+            )
+            status <-
+              tryCatch(
+                system2(
+                  "prefetch",
+                  args = cmd2,
+                  stdout = FALSE,
+                  stderr = FALSE
+                ),
+                error = function(err) {
+                  1
+                },
+                warning = function(war) {
+                  2
+                }
+              )
+            if (status == 0) {
+              file_path <- paste0(dir.download, "/", f)
+              # print(file_path)
+              file <-
+                list.files(
+                  file_path,
+                  pattern = ".sra$",
+                  recursive = FALSE,
+                  full.names = TRUE
+                )
+              #print(file)
+              files <- paste(file, collapse = " ")
+              cmd4 <- paste("mv", files, "-t", dir)
+              system(cmd4)
+            }
+          }
+          unlink(file_path)
+        }
+        if (status != 0)
+          print(
+            "Poor internet connection. The download of RNAseq data failed. Please try it later."
           )
-        #print(file)
-        files <- paste(file,collapse=" ")
-        cmd4 <- paste("mv", files, "-t", dir)
-        system(cmd4)
-        unlink(file_path)
-      }
-    }
+      } else
+        print("Entrez Direct is not found. Please install it.")
+    } else
+      print("SRA Toolkit is not found. Please install it.")
   }
-  if (status > 0)
-    print("Poor internet connection. The download of RNAseq data failed. Please try it later.")
   return(status)
 }
 
@@ -212,4 +279,3 @@ sra_download <- function(accession, dir = getwd())
 # }
 #     }
 # }
-
