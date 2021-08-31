1f5649a2f0f00f302b3612986612e92f16797ee1
sunshanwen <sunshanwen@gmail.com>
2021-08-31 10:50:00 +0800

    update down_Ref based on new datasets api

--- R/down_Ref.R
+++ R/down_Ref.R
@@ -42,7 +42,7 @@ down_Ref <- function(taxa) {
     annotation <- paste0(taxa_tmp, ".gff")
     
     if (!(file.exists(genome) && file.exists(transcript) && file.exists(annotation))) {
-        taxa <- paste0("\"", taxa, "\"")
+        taxa <- paste0("'", taxa, "'")
         datasets <- list.files(pattern = "^datasets$", full.names = TRUE)
         if (length(datasets) == 0) {
             print('Downloading ncbi datasets.')
@@ -64,7 +64,7 @@ down_Ref <- function(taxa) {
               })
               datasets <- list.files(pattern = "^datasets$", full.names = TRUE)
             }
-            if (!status){
+            if (status){
                 print("The internet connection is poor. The download of datasets is failed. Please retry later.")
                 unlink(datasets)
                 return(status)
@@ -72,20 +72,32 @@ down_Ref <- function(taxa) {
         }
         cmd0 <- paste("+x", datasets)
         system2(command = "chmod", args = cmd0)
-        if (dir.exists("dehydrated") && (length(dir(path = "dehydrated", all.files = FALSE)) > 0)) {
-          print("Downloading the reference genome and annotation files.")
-          cmd3 <- paste("rehydrate --directory dehydrated")
-          status <- system2(command = datasets, args = cmd3, stdout = FALSE, stderr = FALSE)
-        } else {
-          cmd2 <- paste("download genome taxon", taxa, "--reference --dehydrated --filename dehydrated.zip")
-          system2(command = datasets, args = cmd2, stdout = TRUE)
-          utils::unzip("dehydrated.zip", list = FALSE, exdir = "dehydrated")
-          print("Downloading the reference genome and annotation files.")
-          cmd3 <- paste("rehydrate --directory ./dehydrated")
-          status <- system2(command = datasets, args = cmd3)
+        i = 0
+        while (!file.exists("dehydrated/ncbi_dataset/fetch.txt")&& i <3) {
+            unlink('dehydrated', recursive = TRUE)
+            cmd2 <- paste("download genome taxon", taxa, "--reference --dehydrated --filename dehydrated.zip")
+            print(cmd2)
+            print(datasets)
+            system2(command = datasets, args = cmd2, stdout = TRUE)
+            if (Sys.info()["sysname"] == "Linux") {
+                system2(command = 'unzip', args = 'dehydrated.zip -d dehydrated', stdout = TRUE)
+            } else{
+                utils::unzip("dehydrated.zip", list = FALSE, overwrite = TRUE, unzip = "internal", exdir = "dehydrated")
+                }
+            i = i + 1
+        } 
+        if(!file.exists("dehydrated/ncbi_dataset/fetch.txt")){
+            status = 1
+            print("The internet connection is poor and the download of reference genome and annotation files failed. Please retry later!")
+            return(status)
         }
+        print("Downloading the reference genome and annotation files.")
+        cmd3 <- paste("rehydrate --directory ./dehydrated")
+        # status <- system2(command = datasets, args = cmd3, stdout = FALSE, stderr = FALSE)
+        status <- system2(command = datasets, args = cmd3)
         
-        if (!status) {
+        # print(status)
+        if (status) {
             print("The internet connection is poor and the download of reference genome and annotation files failed. Please retry later!")
         } else {
           unlink(datasets)
